a52ef95da789fef93eb47464d3f77081e03800cd
shivamjha121 <jha.shivam.121@gmail.com>
2024-03-23 16:57:10 +0530

    first commit

--- passport.auth.ts
+++ passport.auth.ts
@@ -2,7 +2,7 @@ import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import { Strategy as FacebookStrategy } from 'passport-facebook';
 import { Strategy as LocalStrategy } from 'passport-local';
-import usersData from "./model/users.model";
+import usersData from "./model/users.model.js";
 // var GoogleStrategy = Google.Strategy;
 
 
